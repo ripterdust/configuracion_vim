2d276a188a656a5717ea394648a53c21e046d8dc
ripterdust <bryantello2010@hotmail.com>
2022-12-09 20:22:08 -0600

    :rocket: configurado lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,4 +4,4 @@ local capabilities = require'cmp_nvim_lsp'.default_capabilities()
 lsp.pyright.setup{}
 lsp.tsserver.setup{ capabilities = capabilities }
 lsp.html.setup{ capabilities = capabilities }
-lsp.cssls.setup{}
+lsp.cssls.setup{ capabilities = capabilities }
